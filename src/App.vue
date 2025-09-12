fd30a427e4aeb4f55609f6904cc5241964e6f313
chen <1658086552@qq.com>
2025-09-12 13:59:16 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { onMounted } from 'vue'
+import Loading from './components/Loading.vue'
+import Wallpaper from './components/Wallpaper.vue'
+onMounted(() => {
+  // 控制台输出
+  const styleTitle1 = 'font-size: 20px;font-weight: 600;color: rgb(244,167,89);'
+  const styleTitle2 = 'font-size:12px;color: rgb(244,167,89);'
+  const styleContent = 'color: rgb(30,152,255);'
+  const title1 = '流明灵石的主页'
+  const title2 = `
+
+   ____  _   ______  __________  _________   __
+  / __ \\/ | / / /\\ \\/ / ____/ / / / ____/ | / /
+ / / / /  |/ / /  \\  / /   / /_/ / __/ /  |/ / 
+/ /_/ / /|  / /___/ / /___/ __  / /___/ /|  /  
+\\____/_/ |_/_____/_/\\____/_/ /_/_____/_/ |_/   
+
+`
+  const content = `更新日期：2025-09-01`
+  console.info(`%c${title1} %c${title2} %c${content}`, styleTitle1, styleTitle2, styleContent)
+})
+</script>
+
+<template>
+  <Loading />
+  <Wallpaper />
+</template>
+
+<style scoped></style>
---
+++ src/components/Loading.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="loading" :class="store.imgLoaded ? 'hide' : ''">
+    <div class="loading-cover"></div>
+    <div class="loading-icon"><DomainIcon /></div>
+  </div>
+</template>
+<script setup>
+import { store } from '@/store/store.js'
+import DomainIcon from './icons/IconDomain.vue'
+</script>
+<style scoped>
+.loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  opacity: 1;
+
+  &.hide {
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  .loading-cover {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 998;
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+
+  .loading-icon {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    :deep(path) {
+      stroke-dasharray: 2400;
+      stroke-dashoffset: 2400;
+      fill: transparent;
+      animation: draw 3s linear infinite both;
+      stroke-width: 2px;
+      stroke: white;
+    }
+  }
+}
+
+@keyframes draw {
+  0% {
+    stroke-dashoffset: 1px;
+    stroke-dasharray: 0 2400;
+    opacity: 0;
+  }
+  10% {
+    opacity: 1;
+  }
+  45% {
+    stroke-dasharray: 2400 0;
+  }
+
+  65% {
+    stroke-dasharray: 2400 0;
+  }
+  95%,
+  to {
+    stroke-dasharray: 0 2400;
+  }
+}
+</style>
